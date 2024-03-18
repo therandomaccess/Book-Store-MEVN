96a83e3fb99e3ffe5942c75e71f471792cbacf01
Fatih <128058042+therandomaccess@users.noreply.github.com>
2024-03-18 16:09:37 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,20 @@ export default {
   components: {
     NavBar,
   },
+  data() {
+    return {};
+  },
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap");
 
-@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap');
-
-html,body{
+html,
+body {
   font-family: "Mulish", sans-serif;
   font-optical-sizing: auto;
-  font-weight: <weight>;
+  font-weight: 400;
   font-style: normal;
 }
 </style>
