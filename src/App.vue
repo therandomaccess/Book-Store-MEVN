e0fd51c1802524ddbab747d5d9bdcab39dacf464
Fatih <128058042+therandomaccess@users.noreply.github.com>
2024-03-18 15:53:23 +0300

    dosya yapısı

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <NavBar />
+</template>
+
+<script>
+import NavBar from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap');
+
+html,body{
+  font-family: "Mulish", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+}
+</style>
